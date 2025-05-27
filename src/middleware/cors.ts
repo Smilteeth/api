8746dd7b914b5b0f888c5cd247c0f13a77b163aa
Rafael Beltran <rafabeltrans17@gmail.com>
2025-05-27 09:20:54 -0600

    cors

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -2,6 +2,8 @@ import { cors } from 'hono/cors';
 
 export function appCors() {
 	return cors({
-		origin: ['http://localhost:3000/', 'http://localhost:5173/', 'http://127.0.0.1:3000/', 'http://127.0.0.1:5173/']
+		origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://127.0.0.1:5173'],
+		allowMethods: ['GET', 'PUT', 'OPTIONS'],
+		allowHeaders: ['Content-Type']
 	});
 }
