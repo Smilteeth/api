3fbc2024e90e32c5fc4925e054d0506b206c4436
Rafael Beltran <rafabeltrans17@gmail.com>
2025-06-02 20:05:34 -0600

    fixed query for fetching appointmet data

--- src/modules/appointment/appointment.dao.ts
+++ src/modules/appointment/appointment.dao.ts
@@ -1,69 +1,96 @@
 import { AppointmentData, AppointmentTableTypes, DeactiveAppointmentTableTypes } from './appointment.types';
-import { appointmentTable, deactiveAppointmentTable, childTable } from '../../config/db/schema';
+import { appointmentTable, deactiveAppointmentTable, childTable, dentistTable, userTable } from '../../config/db/schema';
 import { DrizzleD1Database } from 'drizzle-orm/d1';
 import { DataAccessObject } from '../../types/daos.interface';
 
 import * as schema from '../../config/db/schema';
-import { and, eq, getTableColumns, or } from 'drizzle-orm';
+import { and, eq, getTableColumns, or, sql } from 'drizzle-orm';
 
-export class AppointmentDao implements DataAccessObject<AppointmentTableTypes> {
-	private db: DrizzleD1Database<typeof schema>;
+export class AppointmentDao implements DataAccessObject<AppointmentTableTypes | AppointmentData> {
+  private db: DrizzleD1Database<typeof schema>;
 
-	constructor(db: DrizzleD1Database<typeof schema>) {
-		this.db = db;
-	}
+  constructor(db: DrizzleD1Database<typeof schema>) {
+    this.db = db;
+  }
 
-	async create(
-		data: Omit<AppointmentTableTypes, 'appointmentId' | 'creationDate' | 'lastModificationDate' | 'isActive'>
-	): Promise<void> {
-		await this.db.insert(appointmentTable).values(data);
-	}
+  async create(
+    data: Omit<AppointmentTableTypes, 'appointmentId' | 'creationDate' | 'lastModificationDate' | 'isActive'>
+  ): Promise<void> {
+    await this.db.insert(appointmentTable).values(data);
+  }
 
-	async fetchUserAppointments(id: number): Promise<Array<AppointmentData> | undefined> {
-		return await this.appointmentQuery(id);
-	}
+  async fetchUserAppointments(id: number): Promise<Array<AppointmentData> | undefined> {
+    const allAppointmentTables = getTableColumns(appointmentTable);
 
-	async fetchById(id: number): Promise<AppointmentData | undefined> {
-		const appointment = await this.appointmentQuery(id);
+    const {
 
-		return appointment[0];
-	}
+      lastModificationDate,
+      ...returnedAppointmentData
+    } = allAppointmentTables;
 
-	// operations with inactive appointment
-	async deactivateAppointment(data: Omit<DeactiveAppointmentTableTypes, 'deactivationDate'>) {
-		await this.db.batch([
-			this.db
-				.update(appointmentTable)
-				.set({ isActive: false })
-				.where(
-					and(eq(appointmentTable.appointmentId, data.deactiveAppointmentId), eq(appointmentTable.isActive, true))
-				),
 
-			this.db.insert(deactiveAppointmentTable).values(data)
-		]);
-	}
+    return await this.db
+      .select({
+        ...returnedAppointmentData,
+        child: sql<string>`concat(${childTable.name}, " ", ${childTable.lastName})`.as('child'),
+        dentist: sql<string>`concat("Dr.", " ", ${userTable.name}, " ", ${userTable.lastName})`.as('dentist')
+      })
+      .from(appointmentTable)
+      .innerJoin(childTable, eq(childTable.childId, appointmentTable.childId))
+      .leftJoin(dentistTable, eq(childTable.dentistId, dentistTable.userId))
+      .leftJoin(userTable, eq(dentistTable.userId, userTable.userId))
+      .where(or(eq(appointmentTable.fatherId, id), eq(appointmentTable.dentistId, id)));
 
-	async fetchUserDeactiveAppointments(id: number): Promise<Array<DeactiveAppointmentTableTypes> | undefined> {
-		return await this.db
-			.select()
-			.from(deactiveAppointmentTable)
-			.where(
-				and(
-					or(eq(appointmentTable.fatherId, id), eq(appointmentTable.dentistId, id)),
-					eq(appointmentTable.appointmentId, deactiveAppointmentTable.deactiveAppointmentId)
-				)
-			);
-	}
+  }
 
-	private async appointmentQuery(id: number): Promise<Array<AppointmentData> | undefined> {
-		return await this.db
-			.select({
-				...getTableColumns(appointmentTable),
-				childName: childTable.name,
-				childLastname: childTable.lastName
-			})
-			.from(appointmentTable)
-			.innerJoin(childTable, eq(childTable.childId, appointmentTable.childId))
-			.where(or(eq(appointmentTable.fatherId, id), eq(appointmentTable.dentistId, id)));
-	}
+  async fetchById(id: number, userId: number): Promise<AppointmentData | undefined> {
+    const allAppointmentTables = getTableColumns(appointmentTable);
+
+    const {
+      lastModificationDate,
+      ...returnedAppointmentData
+    } = allAppointmentTables;
+
+    const appointment = await this.db
+      .select({
+        ...returnedAppointmentData,
+        child: sql<string>`concat(${childTable.name}, " ", ${childTable.lastName})`.as('child'),
+        dentist: sql<string>`concat("Dr.", " ", ${userTable.name}, " ", ${userTable.lastName})`.as('dentist')
+      })
+      .from(appointmentTable)
+      .innerJoin(childTable, eq(childTable.childId, appointmentTable.childId))
+      .leftJoin(dentistTable, eq(childTable.dentistId, dentistTable.userId))
+      .leftJoin(userTable, eq(dentistTable.userId, userTable.userId))
+      .where(and(eq(appointmentTable.appointmentId, id), or(eq(childTable.fatherId, userId), eq(childTable.dentistId, userId))))
+      .limit(1);
+
+    return appointment[0];
+  }
+
+  // operations with inactive appointment
+  async deactivateAppointment(data: Omit<DeactiveAppointmentTableTypes, 'deactivationDate'>) {
+    await this.db.batch([
+      this.db
+        .update(appointmentTable)
+        .set({ isActive: false })
+        .where(
+          and(eq(appointmentTable.appointmentId, data.deactiveAppointmentId), eq(appointmentTable.isActive, true))
+        ),
+
+      this.db.insert(deactiveAppointmentTable).values(data)
+    ]);
+  }
+
+  async fetchUserDeactiveAppointments(id: number): Promise<Array<DeactiveAppointmentTableTypes> | undefined> {
+    return await this.db
+      .select()
+      .from(deactiveAppointmentTable)
+      .where(
+        and(
+          or(eq(appointmentTable.fatherId, id), eq(appointmentTable.dentistId, id)),
+          eq(appointmentTable.appointmentId, deactiveAppointmentTable.deactiveAppointmentId)
+        )
+      );
+  }
+
 }
--- src/modules/appointment/appointment.types.ts
+++ src/modules/appointment/appointment.types.ts
@@ -3,7 +3,7 @@ import { dbTypes } from '../../config/db/types';
 export type AppointmentTableTypes = dbTypes['AppointmentTable'];
 export type DeactiveAppointmentTableTypes = dbTypes['DeactiveAppointmentTable'];
 
-export type AppointmentData = AppointmentTableTypes & {
-	childName: string;
-	childLastname: string;
+export type AppointmentData = Omit<AppointmentTableTypes, "lastModificationDate"> & {
+  child: string;
+  dentist: string;
 };
