0c62f26ffc8d1d4f71f1409f15eb0ff524eeec3a
Rafael Beltran <rafabeltrans17@gmail.com>
2025-06-04 18:38:24 -0600

    endpoint to check if dentist form is filled

--- src/modules/dentist/dentist.service.ts
+++ src/modules/dentist/dentist.service.ts
@@ -6,69 +6,85 @@ import { Pagination, PaginationType } from '../../utils/pagination';
 import { UserTableTypes } from '../auth/auth.types';
 
 export class DentistService {
-	private dentistDao: DentistDao;
-	private jwtPayload: JwtPayload;
-	private pagination: Pagination;
+  private dentistDao: DentistDao;
+  private jwtPayload: JwtPayload;
+  private pagination: Pagination;
 
-	constructor(dao: DentistDao, jwtPayload: JwtPayload) {
-		this.dentistDao = dao;
-		this.jwtPayload = jwtPayload;
-		this.pagination = new Pagination();
-	}
+  constructor(dao: DentistDao, jwtPayload: JwtPayload) {
+    this.dentistDao = dao;
+    this.jwtPayload = jwtPayload;
+    this.pagination = new Pagination();
+  }
 
-	async create(data: Omit<DentistTableTypes, 'userId'>) {
-		if (this.jwtPayload.type !== 'DENTIST') {
-			throw new HTTPException(403, { message: 'User is not a dentist' });
-		}
+  async create(data: Omit<DentistTableTypes, 'userId'>) {
+    if (this.jwtPayload.type !== 'DENTIST') {
+      throw new HTTPException(403, { message: 'User is not a dentist' });
+    }
 
-		await this.dentistDao.create({
-			userId: this.jwtPayload.userId,
-			...data
-		});
-	}
+    await this.dentistDao.create({
+      userId: this.jwtPayload.userId,
+      ...data
+    });
+  }
 
-	async fetchDentists(
-		page: number,
-		limit: number
-	): Promise<
-		PaginationType<
-			Omit<DentistTableTypes, 'university' | 'about'> &
-				Omit<
-					UserTableTypes,
-					'birthDate' | 'password' | 'creationDate' | 'lastModificationDate' | 'isActive' | 'type' | 'email'
-				>
-		>
-	> {
-		const dentists = await this.dentistDao.fetchDentists();
+  async fetchDentists(
+    page: number,
+    limit: number
+  ): Promise<
+    PaginationType<
+      Omit<DentistTableTypes, 'university' | 'about'> &
+      Omit<
+        UserTableTypes,
+        'birthDate' | 'password' | 'creationDate' | 'lastModificationDate' | 'isActive' | 'type' | 'email'
+      >
+    >
+  > {
+    const dentists = await this.dentistDao.fetchDentists();
 
-		if (!dentists) {
-			throw new HTTPException(404, { message: 'Dentists not found' });
-		}
+    if (!dentists) {
+      throw new HTTPException(404, { message: 'Dentists not found' });
+    }
 
-		return this.pagination.generate<
-			Omit<DentistTableTypes, 'university' | 'about'> &
-				Omit<
-					UserTableTypes,
-					'birthDate' | 'password' | 'creationDate' | 'lastModificationDate' | 'isActive' | 'type' | 'email'
-				>
-		>(dentists, page, limit);
-	}
+    return this.pagination.generate<
+      Omit<DentistTableTypes, 'university' | 'about'> &
+      Omit<
+        UserTableTypes,
+        'birthDate' | 'password' | 'creationDate' | 'lastModificationDate' | 'isActive' | 'type' | 'email'
+      >
+    >(dentists, page, limit);
+  }
 
-	async fetchById(
-		childId: number
-	): Promise<
-		Omit<
-			DentistTableTypes &
-				Omit<UserTableTypes, 'birthDate' | 'password' | 'creationDate' | 'lastModificationDate' | 'isActive' | 'type'>,
-			'lastModificationDate'
-		>
-	> {
-		const dentist = await this.dentistDao.fetchById(childId);
+  async fetchById(
+    dentistId: number
+  ): Promise<
+    Omit<
+      DentistTableTypes &
+      Omit<UserTableTypes, 'birthDate' | 'password' | 'creationDate' | 'lastModificationDate' | 'isActive' | 'type'>,
+      'lastModificationDate'
+    >
+  > {
+    const dentist = await this.dentistDao.fetchById(dentistId);
 
-		if (!dentist) {
-			throw new HTTPException(404, { message: 'Dentist not found' });
-		}
+    if (!dentist) {
+      throw new HTTPException(404, { message: 'Dentist not found' });
+    }
 
-		return dentist;
-	}
+    return dentist;
+  }
+
+  async isFormFilled(): Promise<boolean> {
+    try {
+      if (this.jwtPayload.type === "FATHER") {
+        throw new HTTPException(401, { message: "Father doen't need to fill a form" });
+      }
+
+      await this.fetchById(this.jwtPayload.userId);
+      return true;
+    } catch (e) {
+
+      return false;
+    }
+
+
+  }
 }
