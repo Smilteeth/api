7fe7b8c164e4263dee69c12dc03796319bb0d82e
Rafael Beltran <rafabeltrans17@gmail.com>
2025-06-04 22:47:31 -0600

    added endpoints for courses

--- src/core/dao.factory.ts
+++ src/core/dao.factory.ts
@@ -4,39 +4,42 @@ import { AuthDao } from '../modules/auth/auth.dao';
 import { DentistDao } from '../modules/dentist/dentist.dao';
 import { AppointmentDao } from '../modules/appointment/appointment.dao';
 import { ChildDao } from '../modules/child/child.dao';
+import { CourseDao } from '../modules/course/course.dao';
 
-type DaoType = 'auth' | 'dentist' | 'appointment' | 'child';
+type DaoType = 'auth' | 'dentist' | 'appointment' | 'child' | 'course';
 
 interface DaoMap {
-	auth: AuthDao;
-	dentist: DentistDao;
-	appointment: AppointmentDao;
-	child: ChildDao;
+  auth: AuthDao;
+  dentist: DentistDao;
+  appointment: AppointmentDao;
+  child: ChildDao;
+  course: CourseDao;
 }
 
 export class DaoFactory {
-	private daoCreator: {
-		[key in DaoType]: () => DaoMap[key];
-	};
+  private daoCreator: {
+    [key in DaoType]: () => DaoMap[key];
+  };
 
-	constructor(context: Context) {
-		const db = getDb(context.env);
+  constructor(context: Context) {
+    const db = getDb(context.env);
 
-		this.daoCreator = {
-			auth: () => new AuthDao(db),
-			dentist: () => new DentistDao(db),
-			appointment: () => new AppointmentDao(db),
-			child: () => new ChildDao(db)
-		};
-	}
+    this.daoCreator = {
+      auth: () => new AuthDao(db),
+      dentist: () => new DentistDao(db),
+      appointment: () => new AppointmentDao(db),
+      child: () => new ChildDao(db),
+      course: () => new CourseDao(db)
+    };
+  }
 
-	createDao<T extends DaoType>(type: T): DaoMap[T] {
-		const dao = this.daoCreator[type];
+  createDao<T extends DaoType>(type: T): DaoMap[T] {
+    const dao = this.daoCreator[type];
 
-		if (!dao) {
-			throw new Error('Invalid type');
-		}
+    if (!dao) {
+      throw new Error('Invalid type');
+    }
 
-		return dao();
-	}
+    return dao();
+  }
 }
--- src/core/service.factory.ts
+++ src/core/service.factory.ts
@@ -4,41 +4,44 @@ import { DentistService } from '../modules/dentist/dentist.service';
 import { DaoFactory } from './dao.factory';
 import { AppointmentService } from '../modules/appointment/appointment.service';
 import { ChildService } from '../modules/child/child.service';
+import { CourseService } from '../modules/course/course.service';
 
-type ServiceType = 'auth' | 'dentist' | 'appointment' | 'child';
+type ServiceType = 'auth' | 'dentist' | 'appointment' | 'child' | 'course';
 
 interface ServiceMap {
-	auth: AuthService;
-	dentist: DentistService;
-	appointment: AppointmentService;
-	child: ChildService;
+  auth: AuthService;
+  dentist: DentistService;
+  appointment: AppointmentService;
+  child: ChildService;
+  course: CourseService;
 }
 
 export class ServiceFactory {
-	private serviceCreator: {
-		[key in ServiceType]: () => ServiceMap[key];
-	};
+  private serviceCreator: {
+    [key in ServiceType]: () => ServiceMap[key];
+  };
 
-	constructor(context: Context) {
-		const dao = new DaoFactory(context);
+  constructor(context: Context) {
+    const dao = new DaoFactory(context);
 
-		const jwtPayload = context.get('jwtPayload');
+    const jwtPayload = context.get('jwtPayload');
 
-		this.serviceCreator = {
-			auth: () => new AuthService(dao.createDao('auth'), context.env.JWT_SECRET),
-			dentist: () => new DentistService(dao.createDao('dentist'), jwtPayload),
-			appointment: () => new AppointmentService(dao.createDao('appointment'), jwtPayload),
-			child: () => new ChildService(dao.createDao('child'), jwtPayload)
-		};
-	}
+    this.serviceCreator = {
+      auth: () => new AuthService(dao.createDao('auth'), context.env.JWT_SECRET),
+      dentist: () => new DentistService(dao.createDao('dentist'), jwtPayload),
+      appointment: () => new AppointmentService(dao.createDao('appointment'), jwtPayload),
+      child: () => new ChildService(dao.createDao('child'), jwtPayload),
+      course: () => new CourseService(dao.createDao('course'), jwtPayload)
+    };
+  }
 
-	createService<T extends ServiceType>(type: T): ServiceMap[T] {
-		const service = this.serviceCreator[type];
+  createService<T extends ServiceType>(type: T): ServiceMap[T] {
+    const service = this.serviceCreator[type];
 
-		if (!service) {
-			throw new Error('Invalid type');
-		}
+    if (!service) {
+      throw new Error('Invalid type');
+    }
 
-		return service();
-	}
+    return service();
+  }
 }
