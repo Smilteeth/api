f36ed629c8bcee640b189d8a6aa2d2d2b208487a
Rafael Beltran <rafabeltrans17@gmail.com>
2025-06-04 20:31:39 -0600

    change user is available validator

--- src/utils/DateValidator.ts
+++ src/utils/DateValidator.ts
@@ -2,98 +2,98 @@ import { HTTPException } from 'hono/http-exception';
 import { AppointmentTableTypes } from '../modules/appointment/appointment.types';
 
 export enum DateStatus {
-	Past = 'past',
-	Future = 'future',
-	Present = 'present'
+  Past = 'past',
+  Future = 'future',
+  Present = 'present'
 }
 
 export class DateValidator {
-	userIsAvailable(
-		appointments: Array<Omit<AppointmentTableTypes, 'creationDate' | 'lastModificationDate'>> | undefined,
-		appointmentDatetime: string
-	): boolean{
-		if (!appointments) {
-			return true;
-		}
+  userIsAvailable(
+    appointments: Array<Omit<AppointmentTableTypes, 'creationDate' | 'lastModificationDate'>> | undefined,
+    appointmentDatetime: string
+  ): boolean {
+    if (!appointments) {
+      return true;
+    }
 
-		if (appointments.some((appointment) => appointment.appointmentDatetime === appointmentDatetime)) {
-			return false;
-		}
-		return true
-	}
+    if (appointments.some((appointment) => (appointment.appointmentDatetime === appointmentDatetime && !appointment.isActive))) {
+      return false;
+    }
+    return true
+  }
 
-	validFormat(appointmentDatetime: string): boolean {
-		const dateRegex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(?:\.\d{3})?$/;
+  validFormat(appointmentDatetime: string): boolean {
+    const dateRegex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(?:\.\d{3})?$/;
 
-		if (!dateRegex.test(appointmentDatetime)) {
-			return false;
-		}
+    if (!dateRegex.test(appointmentDatetime)) {
+      return false;
+    }
 
-		const appointmentDate = new Date(appointmentDatetime);
+    const appointmentDate = new Date(appointmentDatetime);
 
-		if (isNaN(appointmentDate.getTime())) {
-			return false;
-		}
+    if (isNaN(appointmentDate.getTime())) {
+      return false;
+    }
 
-		const parts = appointmentDatetime.split(/[- :]/).map(Number);
-		const [year, month, day, hour, minute, second] = parts;
+    const parts = appointmentDatetime.split(/[- :]/).map(Number);
+    const [year, month, day, hour, minute, second] = parts;
 
-		if (
-			appointmentDate.getFullYear() !== year ||
-			appointmentDate.getMonth() !== month - 1 ||
-			appointmentDate.getDate() !== day
-		) {
-			return false;
-		}
+    if (
+      appointmentDate.getFullYear() !== year ||
+      appointmentDate.getMonth() !== month - 1 ||
+      appointmentDate.getDate() !== day
+    ) {
+      return false;
+    }
 
-		if (
-			appointmentDate.getHours() !== hour ||
-			appointmentDate.getMinutes() !== minute ||
-			appointmentDate.getSeconds() !== second
-		) {
-			return false;
-		}
+    if (
+      appointmentDate.getHours() !== hour ||
+      appointmentDate.getMinutes() !== minute ||
+      appointmentDate.getSeconds() !== second
+    ) {
+      return false;
+    }
 
-		return true;
-	}
+    return true;
+  }
 
-	isAppointmentDate(appointmentDatetime: string): DateStatus {
-		const appointmentDate = this.getAppointmentDate(appointmentDatetime);
-		const currentDate = this.getCurrentDate();
+  isAppointmentDate(appointmentDatetime: string): DateStatus {
+    const appointmentDate = this.getAppointmentDate(appointmentDatetime);
+    const currentDate = this.getCurrentDate();
 
-		if (appointmentDate.getTime() > currentDate.getTime()) {
-			return DateStatus.Future;
-		} else if (appointmentDate.getTime() < currentDate.getTime()) {
-			return DateStatus.Past;
-		} else {
-			return DateStatus.Present;
-		}
-	}
+    if (appointmentDate.getTime() > currentDate.getTime()) {
+      return DateStatus.Future;
+    } else if (appointmentDate.getTime() < currentDate.getTime()) {
+      return DateStatus.Past;
+    } else {
+      return DateStatus.Present;
+    }
+  }
 
-	appointmentCanBeInactivated(creationDate: string): boolean {
-		const appointmentDate = this.getAppointmentDate(creationDate);
+  appointmentCanBeInactivated(creationDate: string): boolean {
+    const appointmentDate = this.getAppointmentDate(creationDate);
 
-		const currentDate = this.getCurrentDate();
+    const currentDate = this.getCurrentDate();
 
-		const differenceInMs = appointmentDate.getTime() - currentDate.getTime();
+    const differenceInMs = appointmentDate.getTime() - currentDate.getTime();
 
-		const twentyFourHoursInMs = 24 * 60 * 60 * 1000;
+    const twentyFourHoursInMs = 24 * 60 * 60 * 1000;
 
-		// true if is less than 24 hrs
-		return twentyFourHoursInMs > differenceInMs;
-	}
+    // true if is less than 24 hrs
+    return twentyFourHoursInMs > differenceInMs;
+  }
 
-	private getAppointmentDate(appointmentDatetime: string) {
-		const appointmentDate = new Date(appointmentDatetime);
+  private getAppointmentDate(appointmentDatetime: string) {
+    const appointmentDate = new Date(appointmentDatetime);
 
-		if (isNaN(appointmentDate.getTime())) {
-			throw new HTTPException(409, { message: 'Invalid datetime' });
-		}
+    if (isNaN(appointmentDate.getTime())) {
+      throw new HTTPException(409, { message: 'Invalid datetime' });
+    }
 
-		return appointmentDate;
-	}
+    return appointmentDate;
+  }
 
-	private getCurrentDate() {
-		return new Date();
-	}
+  private getCurrentDate() {
+    return new Date();
+  }
 }
